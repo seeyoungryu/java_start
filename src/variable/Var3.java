cdcc59f27871008ca3b52bf531890729250b7ea5
seeyoungryu <seeyoungryu@gmail.com>
2024-04-17 22:27:22 +0900

    update: Variable

---
+++ src/variable/Var3.java
@@ -0,0 +1,28 @@
+package variable;
+
+public class Var3 {
+
+    public static void main(String[] args) {
+
+//변수 타입      - 종류
+        int a; //정수
+        double b; //실수;
+        boolean c; // true, false
+        char d;   //문자
+        String e = "Hello Java";  //문자열
+
+        int f = 1; // -> 개발자가 직접 작성- 리터럴?
+
+
+        //*실무에서 자주 사용하는 변수 타입*
+        //정수: int , long
+        //실수: double
+        //boolean -> 조건문에서 자주 사용
+        //문자열 : String
+
+        //변수 이름-> 소문자로 시작(자바에서 클래스만->대문자로 시작! - 예외: 상수는 모두 대문자),변수이름은 숫자로 시작할 수 없음, 예약어 불가
+//변수 이름 잘 짖는거 중요하다 . 용도와 의미를 명확히 하자!
+        
+
+    }
+}
---
+++ src/variable/Var4.java
@@ -0,0 +1,7 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        
+    }
+}
