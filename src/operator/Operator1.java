d794664527ecab8ec285b04ce2ceb07c3948f0c5
seeyoungryu <seeyoungryu@gmail.com>
2024-04-18 16:10:13 +0900

    add: Operator

---
+++ src/operator/Operator1.java
@@ -0,0 +1,24 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+//        int a, b;
+//        a = 1;
+//        b = 2;
+//
+//        int div = a / b;
+//        System.out.println("div = " + div);
+
+        double a, b;
+        a = 1;
+        b = 2;
+
+        double div = a / b;
+        System.out.println("div = " + div);
+
+
+        // 숫자를 0으로 나눌 수 없음 - > ArithmeticException: by zero 예외 발생하여 해당 시점 이후의 결과가 출력되지 않고 프로그램 종료된다
+
+
+    }
+}
---
+++ src/operator/Operator2.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+
+        //문자열과 숫자 더하면 문자열 됨! (문자열에 더하는것은 다 문자열 되어버림)
+
+
+    }
+}
