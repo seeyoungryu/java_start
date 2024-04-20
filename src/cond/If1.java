3de974e739be996cab9f0d67594e5e80d35c826e
seeyoungryu <seeyoungryu@gmail.com>
2024-04-20 16:17:00 +0900

    add: package cond

---
+++ src/cond/If1.java
@@ -0,0 +1,13 @@
+package cond;
+
+public class If1 {
+    public static void main(String[] args) {
+        //if (조건) {
+//        수행문
+//    }
+
+        //if - else if - esle
+
+        
+    }
+}
